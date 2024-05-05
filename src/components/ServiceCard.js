fec2d4ab300770b17aaaf2bec235b94f2eb9efba
Ozmundooo <28210997+Ozmundooo@users.noreply.github.com.>
2024-05-05 11:10:01 -0400

    new services

--- src/components/ServiceCard.js
+++ src/components/ServiceCard.js
@@ -3,31 +3,47 @@ import PlaceholderImage from "../assets/PlaceholderImage.png";
 import Image from "next/image";
 export default function ServiceCard(props) {
   return (
-    <li className="bg-white   rounded-lg">
+    <li className="bg-white border-[#242051] border-2 rounded-lg">
       <Image
-        className="w-full border-x-2 border-t-2 rounded-x-lg rounded-t-lg border-[#242051]"
-        src={PlaceholderImage}
+        className="w-full  rounded-x-lg rounded-t-lg h-[300px] lg:h-[400px]  object-cover "
+        src={props.image}
       />
-      <div className="px-4 py-4 border-x-2 border-b-2 rounded-x-lg rounded-b-lg border-[#242051]">
-        <div className="bg-[#F0ABAB] px-4 py-2 rounded-full shadow-solid-primary w-fit mb-3">
-          <span className="text-sm font-light lg:text-md xl:text-lg text-[#242051] font-subtitle text-center">
-            ONLINE
-          </span>
+
+      <div className="px-4 py-4  rounded-x-lg rounded-b-lg flex flex-col justify-between ">
+        <div className="">
+          <div className="flex gap-2">
+            {props.online ? (
+              <div className="bg-[#F0ABAB] px-4 py-2 rounded-full shadow-solid-primary w-fit mb-3">
+                <span className="text-sm font-light lg:text-md xl:text-lg text-[#242051] font-subtitle text-center">
+                  ONLINE
+                </span>
+              </div>
+            ) : (
+              <></>
+            )}
+            {props.inperson ? (
+              <div className="bg-[#F0ABAB] px-4 py-2 rounded-full shadow-solid-primary w-fit mb-3">
+                <span className="text-sm font-light lg:text-md xl:text-lg text-[#242051] font-subtitle text-center">
+                  IN PERSON
+                </span>
+              </div>
+            ) : (
+              <></>
+            )}
+          </div>
+          <h3 className="font-title text-[#242051] text-2xl lg:text-2xl xl:text-3xl my-2">
+            {props.title}
+          </h3>
+          <p className="font-subtitle text-[#242051] mb-4">{props.subtitle}</p>
         </div>
-        <h3 className="font-title text-[#242051] text-2xl lg:text-2xl xl:text-3xl my-2">
-          Free 15-Minute Virtual Consultation
-        </h3>
-        <p className="font-subtitle text-[#242051] mb-4">
-          Connect with us for a free 15-minute virtual consultation to discuss
-          your needs and explore how we can support you on your journey to
-          well-being.
-        </p>
-        <a
-          href="#"
-          className="font-subtitle whitespace-nowrap inline-flex justify-center px-8 md:px-14 py-3 md:py-4 border border-transparent rounded-md shadow-sm text-lg md:text-xl  hover:border-white text-white bg-[#221E5A] hover:bg-[#221E5A20] hover:text-[#221E5A]   duration-200 shadow-solid-primary "
-        >
-          Book now
-        </a>
+        <div className="">
+          <a
+            href={props.link}
+            className=" font-subtitle whitespace-nowrap inline-flex items-end justify-center px-8 md:px-14 py-3 md:py-4 border border-transparent rounded-md shadow-sm text-lg md:text-xl  hover:border-white text-white bg-[#221E5A] hover:bg-[#221E5A20] hover:text-[#221E5A]   duration-200 shadow-solid-primary "
+          >
+            Book now
+          </a>
+        </div>
       </div>
     </li>
   );
--- src/pages/services.js
+++ src/pages/services.js
@@ -3,7 +3,11 @@ import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import ServiceCard from "@/components/ServiceCard";
 import Faq from "@/components/Faq";
-
+import Image from "next/image";
+import FreeConsult from "../assets/FreeConsult.jpeg";
+import IndividualTherapy from "../assets/IndividualTherapy.jpeg";
+import StudentTherapy from "../assets/StudentTherapy.jpeg";
+import RateReduced from "../assets/RateReduced.jpeg";
 export default function services() {
   return (
     <>
@@ -17,10 +21,44 @@ export default function services() {
             therapy <span className="font-title">services</span> I offer
           </h2>
           <ul className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3  gap-3">
-            <ServiceCard />
-            <ServiceCard />
-            <ServiceCard />
-            <ServiceCard />
+            <ServiceCard
+              image={FreeConsult}
+              online={true}
+              title={"Free 15-minute virtual consultation"}
+              subtitle={
+                "Connect with us for a free 15-minute virtual consultation to discuss your needs and explore how we can support you on your journey towell-being."
+              }
+              link={"#"}
+            />
+            <ServiceCard
+              image={IndividualTherapy}
+              inperson={true}
+              title={"Individual Therapy (50 minutes)"}
+              subtitle={
+                "Individual therapy is a collaborative process between a therapist and client aimed at exploring thoughts, behaviours, emotions, and experiences. It offers a safe an non-judgemental space to address various concerns and gain insight."
+              }
+              link={"#"}
+            />
+            <ServiceCard
+              image={StudentTherapy}
+              online={true}
+              inperson={true}
+              title={"Student Therapy (50 minutes)"}
+              subtitle={
+                "Student Therapy offers specialized individual counseling tailored to high school and post-secondary students, providing a confidential space to address academic pressure, identity exploration, and mental health concerns like anxiety and depression, as well as navigate the challenges of adolescence and young adulthood with confidence."
+              }
+              link={"#"}
+            />
+            <ServiceCard
+              image={RateReduced}
+              online={true}
+              inperson={true}
+              title={"Reduced-Rate Therapy (50 minutes)"}
+              subtitle={
+                "Rate reductions between $50-$120 are offered based on income and availability. Offered both online and in-person."
+              }
+              link={"#"}
+            />
           </ul>
         </section>
         <section
