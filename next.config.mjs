1354ad8d15f0674b7bda4f3c891aa98b95598b08
Ozmundooo <28210997+Ozmundooo@users.noreply.github.com.>
2024-05-13 13:33:28 -0400

    reverted

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  siteUrl: process.env.SITE_URL || "https://www.freshtajamasi.ca/",
-  generateRobotsTxt: true, // (optional)
-  // ...other options
+  reactStrictMode: true,
 };
 
 export default nextConfig;
