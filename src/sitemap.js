db98ac193024208660fb8038ea58cc5a68997b2f
Ozmundooo <28210997+Ozmundooo@users.noreply.github.com.>
2024-05-13 22:33:44 -0400

    attempt at sitemap

--- src/sitemap.js
+++ src/sitemap.js
@@ -1,5 +1,11 @@
+import { create } from "xmlbuilder2";
+
 export default function sitemap() {
-  return [
+  const xml = create({ version: "1.0", encoding: "UTF-8" }).ele("urlset", {
+    xmlns: "http://www.sitemaps.org/schemas/sitemap/0.9",
+  });
+
+  const urls = [
     {
       url: "https://freshtajamasi.ca/",
       lastModified: new Date(),
@@ -25,4 +31,24 @@ export default function sitemap() {
       priority: 0.5,
     },
   ];
+
+  urls.forEach((url) => {
+    xml
+      .ele("url")
+      .ele("loc")
+      .txt(url.url)
+      .up()
+      .ele("lastmod")
+      .txt(url.lastModified.toISOString())
+      .up()
+      .ele("changefreq")
+      .txt(url.changeFrequency)
+      .up()
+      .ele("priority")
+      .txt(url.priority.toString())
+      .up()
+      .up();
+  });
+
+  return xml.end({ prettyPrint: true });
 }
